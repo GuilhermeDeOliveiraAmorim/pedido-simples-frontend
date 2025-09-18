7b43a25ddca1939e33e821c70c5527ea593e9d20
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2025-09-17 21:04:03 -0300

    feat: adicionar validações de senha e checklist visual no formulário de mudança de senha

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,33 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+function hasMinimumLength(password: string, length: number): boolean {
+  return password.length >= length;
+}
+function hasUpperCaseLetter(password: string): boolean {
+  return /[A-Z]/.test(password);
+}
+function hasLowerCaseLetter(password: string): boolean {
+  return /[a-z]/.test(password);
+}
+function hasDigit(password: string): boolean {
+  return /[0-9]/.test(password);
+}
+function hasSpecialCharacter(password: string): boolean {
+  return /[@#$%&*]/.test(password);
+}
+
+export const passwordChecks = [
+  {
+    check: (pw: string) => hasMinimumLength(pw, 6),
+    label: "Mínimo de 6 caracteres",
+  },
+  { check: hasUpperCaseLetter, label: "Contém letra maiúscula" },
+  { check: hasLowerCaseLetter, label: "Contém letra minúscula" },
+  { check: hasDigit, label: "Contém número" },
+  { check: hasSpecialCharacter, label: "Contém caractere especial (@#$%&*)" },
+];
